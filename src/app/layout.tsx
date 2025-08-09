506792de51de9c44d2b344978a853cb13c0da8fb
kasai22 <65387989+kasai22@users.noreply.github.com>
2025-08-09 19:42:41 +0530

    first commit

---
+++ src/app/layout.tsx
@@ -0,0 +1,37 @@
+import type { Metadata } from "next";
+import { Lora } from "next/font/google";
+import "./globals.css";
+import Navbar from "@/components/Navbar/navbar";
+import Footer from "@/components/Footer/footer";
+import "@fontsource/montaga";
+import GlobalLoader from "@/components/GlobalLoader";
+
+const lora = Lora({
+  variable: "--font-geist-sans",
+  subsets: ["latin"],
+  weight: ["400", "700"],
+});
+
+export const metadata: Metadata = {
+  title: "Hotel First",
+  description: "Hotel First Services",
+};
+
+export default function RootLayout({
+  children,
+}: Readonly<{
+  children: React.ReactNode;
+}>) {
+  return (
+    <html lang="en">
+      <head>
+        <link rel="icon" href="/favicon.svg" type="image/svg+xml" />
+      </head>
+      <body className={`${lora.variable} antialiased`}>
+        <GlobalLoader /> <Navbar />
+        {children}
+        <Footer />
+      </body>
+    </html>
+  );
+}
